85a78add04a2a28bd88584ad423870ca38d7ed12
CharlyNd <10270504+CharlyNd@users.noreply.github.com>
2024-09-22 05:12:59 +0200

    update search style

--- app/(auth)/(tabs)/search.tsx
+++ app/(auth)/(tabs)/search.tsx
@@ -1,5 +1,7 @@
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Image } from 'react-native';
 import React, { useState } from 'react';
+import { ScrollView } from 'react-native-gesture-handler';
+import TrafficLight from '@/components/TrafficLight';
 
 export default function Search() {
   const [name, setName] = useState('');
@@ -8,57 +10,126 @@ export default function Search() {
 
   return (
     <View style={styles.container}>
-      <View style={styles.searchContainer}>
-      <Text>Recherche par Nom et Prénom</Text>
-      <TextInput
-        style={styles.searchBar}
-        placeholder="Rechercher..."
-        value={name}
-        onChangeText={setName}
-      />
-      </View>
-      <View style={styles.searchContainer}>
-      <Text>Recherche par Téléphone</Text>
-      <TextInput
-        style={styles.searchBar}
-        placeholder="Rechercher..."
-        value={phone}
-        onChangeText={setPhone}
-      />
-      </View>
-      <View style={styles.searchContainer}>
-      <Text>Recherche par Email</Text>
-      <TextInput
-        style={styles.searchBar}
-        placeholder="Rechercher..."
-        value={mail}
-        onChangeText={setMail}
-      />
-      </View>
-    </View>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }} scrollEnabled={false} style={styles.subContainer} automaticallyAdjustKeyboardInsets>
+        <View style={styles.results}>
+          <View style={styles.profilContainer}>
+            <View style={{ width: '20%' }}>
+              <TrafficLight color="couple" />
+            </View>
+            <View style={styles.imageSection}>
+              {/* {image && <Image source={{ uri: image }} style={styles.avatar} />} */}
+              {/* {!image && <View style={styles.avatar} />} */}
+              <View style={styles.avatar} />
+              <View style={styles.profilDetails}>
+                <Text style={{ fontSize: 23, fontFamily: "SpaceMono-Regular" }}>Charles-Etienne N'DIAYE</Text>
+                <Text style={{ fontSize: 18, fontFamily: "SpaceMono-Regular" }}>06 12 34 56 78</Text>
+              </View>
+
+            </View>
+
+          </View>
+          {/* <Image source={require('../../../assets/images/noResults.png')} style={styles.image} />
+          <Text style={{ fontSize: 20, color: "#9f9f9f", marginTop: 10 }}>Aucun Résultat</Text> */}
+        </View>
+        <View style={styles.searchSection}>
+          <View style={styles.searchContainer}>
+            <Text style={styles.subtitle}>Recherche par Nom et Prénom</Text>
+            <TextInput
+              style={styles.searchBar}
+              placeholder="Rechercher..."
+              value={name}
+              onChangeText={setName}
+            />
+          </View>
+          <View style={styles.searchContainer}>
+            <Text style={styles.subtitle}>Recherche par Téléphone</Text>
+            <TextInput
+              style={styles.searchBar}
+              placeholder="Rechercher..."
+              value={phone}
+              onChangeText={setPhone}
+            />
+          </View>
+        </View>
+      </ScrollView>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: 'flex-end',
     alignItems: 'center',
-    padding: 16,
+    padding: 20,
   },
-  searchContainer:{
+  avatar: {
+    width: 125,
+    height: 125,
+    backgroundColor: '#ccc',
+    alignSelf: 'center',
+    borderRadius: 100,
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+  subContainer: {
+    width: '100%',
+    paddingHorizontal: 10,
+  },
+  subtitle: {
+    fontFamily: 'SpaceMono-Regular',
+    fontSize: 16,
+  },
+  searchSection: {
+    width: '100%',
+    marginTop: "5%",
+    height: '25%',
+    justifyContent: 'space-around',
+  },
+  searchContainer: {
     width: '100%',
     alignItems: 'center',
-    marginVertical: 16,
   },
   searchBar: {
     backgroundColor: '#f9f9f9',
     height: 40,
-    borderColor: '#ccc',
-    borderWidth: 1,
     borderRadius: 40,
     paddingHorizontal: 15,
     width: '100%',
-    marginTop: 16,
+    marginTop: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62
+  },
+  results: {
+    // marginTop: "10%",
+    // backgroundColor: '#eeeeee',
+    width: '100%',
+    height: '70%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+  },
+  profilContainer: {
+    width: '100%',
+    height: '50%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageSection: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    marginTop: 30,
+    alignItems: "center",
+  },
+  profilDetails: {
+    maxWidth: '60%',
+    marginTop: 20,
   },
 });
